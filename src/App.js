53bc37572a56b9870382895f38835985d56205b9
Junior Trojilio <junior@Juniors-MacBook-Pro.local>
2020-05-07 19:20:45 -0300

    Challenge Solved

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+  const [repositories, setRepositories ] = useState([])  
+  
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+  
+  async function handleAddRepository() {  
+    const response = await api.post('repositories', {
+      title : `Novo Projeto ${Date.now()}`,
+      url : "https://github.com/Rocketseat/umbriel",
+      techs : [
+        "Node",
+        "Express",
+        "TypeScript"
+      ]
+    })
 
-  async function handleRemoveRepository(id) {
-    // TODO
+    const repositorie = response.data
+
+    setRepositories([...repositories, repositorie])
+  }
+  
+
+  async function handleRemoveRepository(id) {  
+      const response = await api.delete(`repositories/${ id }`)
+
+      if(response.status !== 204){
+        return alert("Id cannot be find")
+      }
+
+      const repositorieIndex = repositories.findIndex( repositorie => repositorie.id === id)
+      repositories.splice(repositorieIndex, 1)
+
+      setRepositories([...repositories])
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repositorie => <li key={ repositorie.id }>
+          { repositorie.title }
+          <button onClick={() => handleRemoveRepository(repositorie.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
